b160357edcd42a2112abd38c67e6a1abd671037f
Gabriel Ferreira <gabrielferreira019@gmail.com>
2025-07-02 10:42:40 -0300

    refactor: create function to convert number to BRL

--- src/utils/string.utils.ts
+++ src/utils/string.utils.ts
@@ -1,3 +1,10 @@
 export function standardize(str: string) {
     return str.toUpperCase().replace(/\W+/g, "");
 }
+
+export function formatToBRL(value: number): string {
+    return new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    }).format(value);
+}
